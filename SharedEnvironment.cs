5b499d7dbba4312e98dc94dffeda1802497d48ac
Konfirm44 <tomsitek@gmail.com>
2023-01-28 21:48:40 +0100

    working except input live updates

--- SharedEnvironment.cs
+++ SharedEnvironment.cs
@@ -1,10 +1,34 @@
-﻿namespace ShaREPL;
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace ShaREPL;
 
 public class SharedEnvironment
 {
-    public Guid Guid { get; init; } = new();
+    public Guid Guid { get; init; } = Guid.NewGuid();
 
-    public List<string> Input { get; init; } = new();
-    public List<string> Output { get; init; } = new();
+    private List<string> InputArchive { get; init; } = new();
+    public IReadOnlyList<string> Output { get; init; } = new List<string>();
+    public string Input { get; set; } = "";
+
+    public void OnInput(ChangeEventArgs e)
+    {
+        Input = e.Value?.ToString() ?? "";
+        Update?.Invoke(default, EventArgs.Empty);
+    }
+
+    public void OnEnter(KeyboardEventArgs e)
+    {
+        if (e.Key == "Enter")
+        {
+            Input += "\n";
+            InputArchive.Add(Input); 
+            (Output as List<string>)!.Add(Input);
+            Input = "";
+            Update?.Invoke(default, EventArgs.Empty);
+        }
+    }
+
+    public event EventHandler? Update;
 
 }
