c04b47f984f0afa2aad866e7e285548cc2bd1ac7
Konfirm44 <tomsitek@gmail.com>
2023-01-30 23:42:35 +0100

    simple repl works

---
+++ Repl.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
+
+namespace ShaREPL;
+
+public class Repl
+{
+    ScriptState<object>? state;
+
+    public bool HasState { get => state is not null; }
+
+    public async Task Init()
+    {
+        var opt = ScriptOptions.Default.WithImports("System", "System.Math");
+        state = await CSharpScript.RunAsync("\"hello world\"", opt);
+    }
+
+    public async Task<string> Evaluate(string input)
+    {
+        var result = "";
+        try
+        {
+            state = await state!.ContinueWithAsync(input);
+            result = state.ReturnValue?.ToString() ?? "no return value";
+        }
+        catch (CompilationErrorException ex)
+        {
+            result = ex.Message;
+            //state = null;
+        }
+        return result;
+    }
+}
--- SharedEnvironment.cs
+++ SharedEnvironment.cs
@@ -11,24 +11,50 @@ public class SharedEnvironment
     public IReadOnlyList<string> Output { get; init; } = new List<string>();
     public string Input { get; set; } = "";
 
+    private readonly Repl _repl = new();
+
+    public event EventHandler? Update;
+
+    public SharedEnvironment() 
+    {
+        AddToOutput("Enter C# code:\n");
+    }
+
     public void OnInput(ChangeEventArgs e)
     {
         Input = e.Value?.ToString() ?? "";
         Update?.Invoke(this, EventArgs.Empty);
     }
 
-    public void OnEnter(KeyboardEventArgs e)
+    public async Task OnEnter(KeyboardEventArgs e)
     {
         if (e.Key == "Enter")
         {
-            Input += "\n";
-            InputArchive.Add(Input); 
-            (Output as List<string>)!.Add(Input);
+            if (!_repl.HasState)
+            {
+               await _repl.Init();
+            }
+
+            var input = Input + "\n";
             Input = "";
+            InputArchive.Add(input);
+            AddToOutput(input);
+            Update?.Invoke(this, EventArgs.Empty);
+
+            var output = await ProcessLine(input);
+            AddToOutput(output + "\n");
+            
             Update?.Invoke(this, EventArgs.Empty);
         }
     }
 
-    public event EventHandler? Update;
+    private async Task<string> ProcessLine(string line)
+    {
+        return await _repl.Evaluate(line);
+    }
 
+    private void AddToOutput(string text)
+    {
+        (Output as List<string>)!.Add(text);
+    }
 }
